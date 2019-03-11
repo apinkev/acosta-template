6000b07bde88d715544b6f6024f1212d13760768
Artur <apinkevich@yahoo.com>
2019-03-11 14:44:43 +0200

    styles

---
+++ js/index.js
@@ -0,0 +1,16 @@
+// import React, {Component} from 'react';
+// import ReactDOM from 'react-dom';
+
+// class App extends Component {
+//     constructor() {
+//         super()
+//     }
+
+//     render() {
+//         return (
+//             <h1>Hello World!</h1>
+//         )
+//     }
+// }
+
+// ReactDOM.render(<App />, document.getElementById("root"))
